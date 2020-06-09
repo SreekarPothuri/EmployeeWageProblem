8214d795d5af4df8f7b37c25122ccdc02d7bd9ca
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-09 16:54:14 +0530

    UC-4:Added

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -9,14 +9,16 @@ salary=$(( $wagePerHr * $perDayHrs ))
 
 CheckAttendance=$(( RANDOM%3 ))
 
-if [ $CheckAttendance -eq 1 ]
-then
-	echo "Employee is Present and works Full Time"
-	echo "Salary is : $salary"
-elif [ $CheckAttendance -eq 2 ]
-then
-	echo "Employee is Present and works Part Time"
-	echo "Salary is : $(( $salary / 2 ))"
-else
-	echo "Employee is Absent"
-fi
+case $CheckAttendance in
+	1)
+		echo "Employee is Present and works Full Time"
+		echo "Salary is : $salary"
+		;;
+	2)
+		echo "Employee is Present and works Part Time"
+		echo "Salary is : $(( $salary / 2 ))"
+		;;
+	*)
+		echo "Employee is Absent"
+		;;
+esac
