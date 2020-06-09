114ae2058af01bd8fbbd912ac05a206241f187c8
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-09 18:01:55 +0530

    UC-6:Added

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -4,35 +4,48 @@ echo "Welcome to Employee Wage Computation Program"
 
 wagePerHr=20
 perDayHrs=8
-NumOfWorkingDaysPermonth=20
+max_work_Hrs=100
+max_work_days=20
+
 SalaryPerDay=0
 TotalSalary=0
+WorkingHrsperDay=0
+TotalWorkingHrs=0
 
-for(( i=0; i<=20; i++ ))
+for(( days=0; days<=$max_work_days; days++ ))
 do
-	CheckAttendance=$(( RANDOM%3 ))
+	if [ $TotalWorkingHrs -le 100 ]
+	then
+		CheckAttendance=$(( RANDOM%3 ))
 
-	case $CheckAttendance in
-		1)
-			echo "Employee is Present and works Full Time"
-			SalaryPerDay=$(( $wagePerHr * $perDayHrs ))
-			echo "Salary is : $SalaryPerDay"
-			;;
-		2)
-			echo "Employee is Present and works Part Time"
-			SalaryPerDay=$(( $wagePerHr * $perDayHrs / 2 ))
-			echo "Salary is : $SalaryPerDay"
-			;;
-		0)
-			echo "Employee is Absent"
-			SalaryPerDay=0
-			;;
-	esac
+		case $CheckAttendance in
+			1)
+				echo "Employee is Present and works Full Time"
+				WorkingHrsperDay=8
+				SalaryPerDay=$(( $wagePerHr * $perDayHrs ))
+				echo "Salary for today is : $SalaryPerDay"
+				;;
+			2)
+				echo "Employee is Present and works Part Time"
+				WorkingHrsperDay=4
+				SalaryPerDay=$(( $wagePerHr * $perDayHrs / 2 ))
+				echo "Salary for today is : $SalaryPerDay"
+				;;
+			0)
+				echo "Employee is Absent"
+				WorkingHrsperDay=0
+				SalaryPerDay=0
+				;;
+		esac
 
-	TotalSalary=$(( $TotalSalary + $SalaryPerDay ))
+		TotalSalary=$(( $TotalSalary + $SalaryPerDay ))
+		echo "Total Salary till now is : $TotalSalary"
 
-	echo $TotalSalary
-
+		TotalWorkingHrs=$(( $TotalWorkingHrs + $WorkingHrsperDay ))
+		echo "Total Working Hours : $TotalWorkingHrs"
+		printf "\n"
+	fi
 done
 
 echo "Employee wage for a month is : $TotalSalary"
+
