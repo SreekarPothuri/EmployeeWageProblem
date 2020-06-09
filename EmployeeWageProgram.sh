ad329a10d8d94c30db42bb42bb06e7abcec6f29c
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-09 17:39:29 +0530

    UC-5:Added

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -4,21 +4,35 @@ echo "Welcome to Employee Wage Computation Program"
 
 wagePerHr=20
 perDayHrs=8
+NumOfWorkingDaysPermonth=20
+SalaryPerDay=0
+TotalSalary=0
 
-salary=$(( $wagePerHr * $perDayHrs ))
+for(( i=0; i<=20; i++ ))
+do
+	CheckAttendance=$(( RANDOM%3 ))
 
-CheckAttendance=$(( RANDOM%3 ))
+	case $CheckAttendance in
+		1)
+			echo "Employee is Present and works Full Time"
+			SalaryPerDay=$(( $wagePerHr * $perDayHrs ))
+			echo "Salary is : $SalaryPerDay"
+			;;
+		2)
+			echo "Employee is Present and works Part Time"
+			SalaryPerDay=$(( $wagePerHr * $perDayHrs / 2 ))
+			echo "Salary is : $SalaryPerDay"
+			;;
+		0)
+			echo "Employee is Absent"
+			SalaryPerDay=0
+			;;
+	esac
 
-case $CheckAttendance in
-	1)
-		echo "Employee is Present and works Full Time"
-		echo "Salary is : $salary"
-		;;
-	2)
-		echo "Employee is Present and works Part Time"
-		echo "Salary is : $(( $salary / 2 ))"
-		;;
-	*)
-		echo "Employee is Absent"
-		;;
-esac
+	TotalSalary=$(( $TotalSalary + $SalaryPerDay ))
+
+	echo $TotalSalary
+
+done
+
+echo "Employee wage for a month is : $TotalSalary"
