17cf9052c630dc54d510aca7163a04112870c3e7
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-09 11:26:00 +0530

    UC-1:Added

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Welcome to Employee Wage Computation Program"
+
+CheckAttendance=$(( RANDOM%2 ))
+
+if [ $checkAttendance -eq 1 ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
